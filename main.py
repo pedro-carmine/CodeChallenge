07238f0d66d6177eca2d6ebf58e336f5aeaaeae8
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-10-29 22:31:41 +0100

    adding return 1d

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ def get_names(tickers):
         names.append(data.info['longName'])
     return names
 
+def get_return_1d(data, tickers):
+    returns_1d = []
+    for ticker in tickers:
+        returns_1d.append(data[ticker]['Close'].pct_change()[::-1].head(1).values[0])
+    return returns_1d
+    
+
 tickers = ["META", "AAPL", "MSFT", "TSLA", "AMZN", "SHEL", "NSRGY", "ROG.SW", "OR.PA", "AZN"]
 try:
-    data = yf.download(tickers=tickers, end=datetime.datetime.now(), start=datetime.datetime.now() - relativedelta(months=1))
+    data = yf.download(tickers=tickers, end=datetime.datetime.now(), start=datetime.datetime.now() - relativedelta(months=1), group_by='ticker')
 except Exception as exception:
     print(f"An error occurred while fetching tickers: {exception}")
 
-data = data[::-1].head()
-
+returns_1d = get_return_1d(data, tickers)
 names = get_names(tickers)
+print(returns_1d)
+print(names)
+#print(data['AMZN']['Close'].pct_change(periods=1, freq='M'))
