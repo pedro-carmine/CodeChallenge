c3afff20aea49eb271cae403cbfbe4afe1185a4e
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-10-29 10:07:06 +0100

    adding try except for tickers fetching

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import yfinance as yf
 import pandas as pd
 
 tickers = ["META", "AAPL", "MSFT", "TSLA", "AMZN", "SHEL", "NSRGY", "ROG.SW", "OR.PA", "AZN"]
-tickers = (yf.Ticker(ticker) for ticker in tickers)
+try:
+    tickers = (yf.Ticker(ticker) for ticker in tickers)
+except Exception as exception:
+    print(f"An error occurred while fetching tickers: {exception}")
+
 
 for ticker in tickers:
     print(ticker)
