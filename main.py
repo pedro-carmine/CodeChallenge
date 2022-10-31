67cde6aa77d54031127155d10475d91fb3aea0c4
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-10-31 21:02:14 +0000

    adding dictionary prices

--- main.py
+++ main.py
@@ -54,7 +54,18 @@ def get_last_closes(data, tickers):
     for ticker in tickers:
         last_closes.append(data[ticker]['Close'].head(1).values[0])
     return last_closes
-    
+
+def create_ts(data, tickers):
+    ts = {}
+    for ticker in tickers:
+        pass
+
+def get_prices(data, tickers):
+    prices = {}
+    data = data[::-1]
+    for ticker in tickers:
+        prices[ticker] = data[ticker]['Close'].head(10).values.round(2)
+    return prices
 
 try:
     data = yf.download(tickers=tickers, period='1mo', group_by='ticker')
@@ -62,14 +73,17 @@ except Exception as exception:
     print(f"An error occurred while fetching tickers: {exception}")
 
 
-returns_1d = get_return_1d(data, tickers)
-returns_1w = get_return_1w(data, tickers)
-returns_1m = get_return_1m(data, tickers)
-names = get_names(tickers)
-last_close_dates = get_last_close_dates(data, tickers)
-last_closes = get_last_closes(data, tickers)
-average = get_month_avg(data, tickers)
+# returns_1d = get_return_1d(data, tickers)
+# returns_1w = get_return_1w(data, tickers)
+# returns_1m = get_return_1m(data, tickers)
+# names = get_names(tickers)
+# last_close_dates = get_last_close_dates(data, tickers)
+# last_closes = get_last_closes(data, tickers)
+# average = get_month_avg(data, tickers)
 
-df_data = list(zip(tickers, names, last_close_dates, last_closes, returns_1d, returns_1w, returns_1m, average))
-df = pd.DataFrame(df_data, columns=headers).sort_values(by='Symbol')
-print(df.to_string(index=False))
+# df_data = list(zip(tickers, names, last_close_dates, last_closes, returns_1d, returns_1w, returns_1m, average))
+# df = pd.DataFrame(df_data, columns=headers).sort_values(by='Symbol')
+# print(df.to_string(index=False))
+
+newdict = get_prices(data, tickers)
+print(str(newdict))
