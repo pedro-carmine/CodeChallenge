c6ff68629e57be25fef11695b86a32ff3f16c157
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-11-07 22:31:08 +0000

    final code commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ tickers = ["META", "AAPL", "MSFT", "TSLA", "AMZN", "SHEL", "NSRGY", "ROG.SW", "O
 headers = ['Symbol', 'Name', 'Last Date', 'Last Close', 'Return 1D', 'Return 1W', 'Return 1M', 'Average 1M']
 
 
-def get_names(tickers) -> list:
+def get_names(tickers: list) -> list:
     """Gets the name of the tickers specified in the tickers list.
 
     Args:
@@ -32,7 +32,7 @@ def get_names(tickers) -> list:
     return names
 
 
-def get_last_close_dates(data, tickers) -> list:
+def get_last_close_dates(data: pd.DataFrame, tickers: list) -> list:
     """Gets the last close date for each ticker from the yfinance API.
 
     Args:
@@ -49,7 +49,7 @@ def get_last_close_dates(data, tickers) -> list:
 
 
 @st.cache
-def get_return_1d(data, tickers) -> list:
+def get_return_1d(data: pd.DataFrame, tickers: list) -> list:
     """Gets the value of the return in one day for each ticker.
 
     Args:
@@ -66,7 +66,7 @@ def get_return_1d(data, tickers) -> list:
 
 
 @st.cache
-def get_return_1w(data, tickers) -> list:
+def get_return_1w(data: pd.DataFrame, tickers: list) -> list:
     """Gets the value of the return in one week for each ticker.
 
     Args:
@@ -83,7 +83,7 @@ def get_return_1w(data, tickers) -> list:
 
 
 @st.cache
-def get_return_1m(data, tickers) -> list:
+def get_return_1m(data: pd.DataFrame, tickers: list) -> list:
     """Gets the value of the return in one month for each ticker.
 
     Args:
@@ -100,7 +100,7 @@ def get_return_1m(data, tickers) -> list:
 
 
 @st.cache
-def get_month_avg(data, tickers) -> list:
+def get_month_avg(data: pd.DataFrame, tickers: list) -> list:
     """Sums up all the values and calculate the average value in the month of each ticker.
         This function can probably be improved to calculate the average using numpy.
 
@@ -118,7 +118,7 @@ def get_month_avg(data, tickers) -> list:
     return avg
 
 
-def get_last_closes(data, tickers) -> list:
+def get_last_closes(data: pd.DataFrame, tickers: list) -> list:
     """Gets the value of the last close of each ticker.
 
     Args:
@@ -135,7 +135,15 @@ def get_last_closes(data, tickers) -> list:
     return last_closes
 
 
-def create_ts(tickers):
+def create_ts(tickers: list) -> dict:
+    """Creates a time series with date and
+
+    Args:
+        tickers: List of the strings of the tickers to be fetched.
+
+    Returns:
+        A dictionary containing the close and 1 month return for each ticker
+    """
     ts = {}
     columns_to_be_dropped = ['Open', 'High', 'Low', 'Adj Close', 'Volume']
     for ticker in tickers:
@@ -146,7 +154,16 @@ def create_ts(tickers):
     return ts
 
 
-def get_marketcap(tickers) -> dict:
+def get_marketcap(tickers: list) -> dict:
+    """Gets the marketcap for each country of the tickers.
+
+    Args:
+        tickers: List of the strings of the tickers to be fetched.
+
+    Returns:
+        A dictionary where the keys are the countries and the values are dictionaries
+        containing the marketcap and the count of each country for future calculation.
+    """
     countries_mkcap = {}
     count = 0
 
@@ -156,27 +173,47 @@ def get_marketcap(tickers) -> dict:
         country = info['country']
         mkcap = info['marketCap']
 
-        if country in countries_mkcap:
+        if country in countries_mkcap:  # if the country is already in the dictionary, just sum the market cap and
+            # increase the counter
             current = countries_mkcap[country]['mkcap']
             countries_mkcap[country]['mkcap'] = current + mkcap
             countries_mkcap[country]['count'] += 1
-        else:
+        else:  # creates a sub dictionary and add it to the main dictionary containing the new country
             country_dict = {'mkcap': mkcap, 'count': 1}
             countries_mkcap[country] = country_dict
 
-    countries_mkcap['total_count'] = count
+    countries_mkcap['total_count'] = count  # stores the total count of the market cap (10 in this project) for
+    # future weighted average calculation
     return countries_mkcap
 
 
-def get_prices(data, tickers) -> dict:
+def get_10_last_prices(data: pd.DataFrame, tickers: list) -> dict:
+    """Gets the 10 last close prices for each ticker.
+
+    Args:
+        data: DataFrame containing data about each ticker
+        tickers: List of the strings of the tickers to be fetched.
+
+    Returns:
+        A dictionary where the key is the ticker name and the value is a list with the 10 last close values.
+    """
     prices = {}
-    data = data[::-1]
+    data = data[::-1]  # inverts the list so the most recent closes are listed first.
     for ticker in tickers:
-        prices[ticker] = data[ticker]['Close'].head(10).values.round(2)
+        prices[ticker] = data[ticker]['Close'].head(10).values.round(2)  # filters 10 rows and rounds the value.
     return prices
 
 
 def country_statistics(mkcap: dict) -> dict:
+    """Calculates the statistics for the marketcap of each country.
+
+    Args:
+        mkcap: Dictionary where the key is the country name and the value is a dictionary with the total marketcap
+    and ticker count.
+
+    Returns:
+        A dictionary containing the statistics for each country.
+    """
     statistics = {}
     total_count = mkcap.pop('total_count')  # retrieves values and remove from the dict
 
@@ -186,12 +223,18 @@ def country_statistics(mkcap: dict) -> dict:
     return statistics
 
 
-def chart(ticker, ts):
-    if ticker in tickers:
+def chart(ticker: str, ts: dict) -> None:
+    """Draws a line chart with the specified ticker.
+
+    Args:
+        ticker: String of the user input.
+        ts: Dictionary containing the timeseries information.
+    """
+    if ticker.upper() in tickers:  # if the ticker is listed, it creates the line chart
         st.line_chart(ts[ticker.upper()]['Close'], use_container_width=True)
-    elif ticker == '':
+    elif ticker == '':  # when the input is none
         st.warning("Please insert a ticker")
-    else:
+    else:  # when the input is not valid
         st.error("Invalid ticker name")
 
 
@@ -200,38 +243,52 @@ def load_data(df_data, headers):
     return pd.DataFrame(df_data, columns=headers).sort_values(by='Symbol')
 
 
-try:
-    data = yf.download(tickers=tickers, period='1mo', group_by='ticker')
-except Exception as exception:
-    print("An error occurred while fetching tickers:")
-    raise exception
+@st.cache
+def loadup() -> list:
+    """Loads up and download all the tickers data and process it.
+        Values are cached using the @st.cache notation for streamlit.
 
-returns_1d = get_return_1d(data, tickers)
-returns_1w = get_return_1w(data, tickers)
-returns_1m = get_return_1m(data, tickers)
-names = get_names(tickers)
-last_close_dates = get_last_close_dates(data, tickers)
-last_closes = get_last_closes(data, tickers)
-average = get_month_avg(data, tickers)
+    Returns:
+        List with the dataframe and timeseries that will be displayed in the dashboard.
+    """
+    try:
+        data = yf.download(tickers=tickers, period='1mo', group_by='ticker', progress=False)
+    except Exception as exception:
+        print("An error occurred while fetching tickers:")
+        raise exception
 
-df_data = list(zip(tickers, names, last_close_dates, last_closes, returns_1d, returns_1w, returns_1m, average))
-df = load_data(df_data, headers)
+    returns_1d = get_return_1d(data, tickers)
+    returns_1w = get_return_1w(data, tickers)
+    returns_1m = get_return_1m(data, tickers)
+    names = get_names(tickers)
+    last_close_dates = get_last_close_dates(data, tickers)
+    last_closes = get_last_closes(data, tickers)
+    average = get_month_avg(data, tickers)
 
-# newdict = get_prices(data, tickers)
-# print(str(newdict))
+    df_data = list(zip(tickers, names, last_close_dates, last_closes, returns_1d, returns_1w, returns_1m, average))
+    df = load_data(df_data, headers)
 
-# mkcap = get_marketcap(tickers)
-# stats = country_statistics(mkcap)
+    newdict = get_10_last_prices(data, tickers)
+    print(str(newdict))
 
-# df2 = pd.DataFrame(stats)
-# print(df2.transpose())
+    mkcap = get_marketcap(tickers)
+    stats = country_statistics(mkcap)
 
-st.header("Dystematic Dashboard")
-st.subheader("Companies")
-st.dataframe(df, use_container_width=True)
+    df2 = pd.DataFrame(stats)
+    print(df2.transpose())
 
-ts = create_ts(tickers)
+    ts = create_ts(tickers)
 
-selected = st.text_input(label="Insert ticker")
-st.subheader("Prices")
-chart(selected, ts)
+    return [df, ts]
+
+
+if __name__ == '__main__':
+    [df, ts] = loadup()  # cached data, so it does not process all the data again
+    st.header("Dystematic Dashboard")
+    st.dataframe(df, use_container_width=True)  # main dataframe
+
+    st.subheader("Companies")
+    selected = st.text_input(label="Insert ticker")  # input box
+
+    st.subheader("Prices")
+    chart(selected, ts)  # chart with the timeseries
